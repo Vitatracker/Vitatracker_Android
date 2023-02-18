ae1378e2533b283d92d13986e11d11a1979b5e8a
v <92236559+palevominimale@users.noreply.github.com>
2023-02-18 21:39:09 +0100

    modified methods of api

--- data/src/main/java/app/mybad/data/api/RemoteApi.kt
+++ data/src/main/java/app/mybad/data/api/RemoteApi.kt
@@ -2,6 +2,7 @@ package app.mybad.data.api
 
 import app.mybad.data.models.course.CourseDataModel
 import app.mybad.data.models.med.MedDataModel
+import app.mybad.data.models.usages.UsageDataModel
 import app.mybad.data.models.usages.UsagesDataModel
 import app.mybad.data.models.user.UserDataModel
 
@@ -9,20 +10,18 @@ interface RemoteApi {
 
     //Auth
     fun checkLogin(login: String) : Boolean
-    fun createUser(login: String, passwordHash: String) : String        //API key for user POST
-    fun authUser(login: String, passwordHash: String) : String          //API key for user
-    fun restorePassword(login: String) : Boolean
-    fun setNewPassword(token: String, passwordHash: String) : Boolean   //token will be sent via email (as link) when user trying to restore his password
+    fun createUser(login: String, passwordHash: String)                 //API key for user POST
+    fun authUser(login: String, passwordHash: String)                   //API key for user
+    fun restorePassword(login: String)
+    fun setNewPassword(token: String, passwordHash: String)             //token will be sent via email (as link) when user trying to restore his password
 
     //Onboarding
-    fun setUserModel(model: UserDataModel) : Boolean
+    fun setUserModel(model: UserDataModel)
 
     //Main Screen
-    fun addCourse(
-        course: CourseDataModel,
-        med: MedDataModel,
-        usagesDataModel: UsagesDataModel
-    ) : Boolean                                                         //POST
+    fun addCourse(course: CourseDataModel)                              //POST
+    fun addMed(med: MedDataModel)
+    fun addUsages(usages: UsageDataModel)
 
     fun hasChanges() : Boolean
 
