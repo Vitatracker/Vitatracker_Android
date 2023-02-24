65c3deba8bb64501c23690fa43ac555effd40308
valentynkulahin <valik_white@ukr.net>
2023-02-24 19:42:14 +0100

    added data models (data layer)

--- app/src/main/java/app/mybad/notifier/StartMainScreen.kt
+++ app/src/main/java/app/mybad/notifier/StartMainScreen.kt
@@ -1,6 +1,9 @@
 package app.mybad.notifier
 
+import android.content.res.Resources
+import android.icu.util.Calendar
 import android.os.Build
+import android.util.DisplayMetrics
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -24,19 +27,24 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import app.mybad.notifier.ui.screens.authorization.SurfaceSignInWith
 import app.mybad.notifier.ui.screens.authorization.login.*
 import app.mybad.notifier.ui.screens.authorization.navigation.AuthorizationNavItem
+import app.mybad.notifier.ui.screens.navigation.NavItemMain
 import kotlinx.coroutines.launch
-import java.time.LocalDate
-import java.time.Month
+import java.text.DateFormatSymbols
+import java.time.*
 import kotlin.io.path.ExperimentalPathApi
 import kotlin.math.absoluteValue
+import kotlin.time.Duration.Companion.days
 
 @RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
@@ -55,7 +63,6 @@ fun StartMainScreen(navController: NavHostController) {
                 colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
             )
         },
-        floatingActionButtonPosition = FabPosition.End,
         content = { contentPadding ->
             Surface(
                 modifier = Modifier
@@ -77,9 +84,10 @@ fun MainScreen(navController: NavHostController) {
     ) {
         MainScreenBackgroundImage()
         Column(
-            modifier = Modifier
+            modifier = Modifier, horizontalAlignment = Alignment.CenterHorizontally
         ) {
             MainScreenMonthPager()
+            MainScreenWeekPager()
         }
     }
 
@@ -90,26 +98,29 @@ fun MainScreenBackgroundImage() {
 
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainScreenMonthPager() {
 
-    var monthState by remember { mutableStateOf(0) }
+    val paddingStart = 10.dp
+    val paddingEnd = 10.dp
 
-    val state = rememberPagerState()
+    val state = rememberPagerState(LocalDate.now().month.ordinal)
     val scope = rememberCoroutineScope()
 
     HorizontalPager(
         pageCount = Month.values().size,
         state = state,
-        pageSpacing = 20.dp,
+        pageSpacing = 13.dp,
         pageSize = PageSize.Fixed(40.dp),
-        modifier = Modifier.padding(top = 14.dp, start = 10.dp, end = 10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 14.dp, start = paddingStart, end = paddingEnd),
         verticalAlignment = Alignment.CenterVertically,
-        contentPadding = PaddingValues(horizontal = 170.dp)
+        contentPadding = PaddingValues(
+            horizontal = (Resources.getSystem().configuration.screenWidthDp.dp - paddingStart * 3) / 2
+        )
     ) { month ->
-        monthState = month
         Surface(
             modifier = Modifier.fillMaxWidth(),
             shape = RoundedCornerShape(5.dp)
@@ -146,6 +157,85 @@ fun MainScreenMonthPager() {
                 maxLines = 1,
                 textAlign = TextAlign.Center
             )
+        }
+    }
+
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MainScreenWeekPager() {
+
+    val paddingStart = 10.dp
+    val paddingEnd = 10.dp
+
+    val dayOfWeek = DayOfWeek.values()
+    val shortWeekDays = DateFormatSymbols.getInstance().shortWeekdays
+
+    val state = rememberPagerState(LocalDate.now().dayOfMonth)
+    val scope = rememberCoroutineScope()
+
+    HorizontalPager(
+        pageCount = LocalDate.now().month.maxLength(),
+        state = state,
+        pageSpacing = 13.dp,
+        pageSize = PageSize.Fixed(40.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp, start = paddingStart, end = paddingEnd),
+        verticalAlignment = Alignment.CenterVertically,
+        contentPadding = PaddingValues(
+            horizontal = (Resources.getSystem().configuration.screenWidthDp.dp - paddingStart * 3) / 2
+        )
+    ) { days ->
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(5.dp)
+        ) {
+            Box(modifier = Modifier) {
+                Text(
+                    text = AnnotatedString(days.toString()),
+                    color = if (state.currentPage == days) {
+                        MaterialTheme.colorScheme.primaryContainer
+                    } else {
+                        Color.LightGray
+                    },
+                    modifier = Modifier
+                        .padding(1.dp)
+                        .clickable {
+                            scope.launch { state.animateScrollToPage(days) }
+                        },
+                    fontWeight = if (state.currentPage == days) {
+                        FontWeight.Bold
+                    } else {
+                        FontWeight.Normal
+                    },
+                    maxLines = 1,
+                    textAlign = TextAlign.Center
+                )
+
+                Text(
+                    text = AnnotatedString(days.toString()),
+                    color = if (state.currentPage == days) {
+                        MaterialTheme.colorScheme.primaryContainer
+                    } else {
+                        Color.LightGray
+                    },
+                    modifier = Modifier
+                        .padding(1.dp)
+                        .clickable {
+                            scope.launch { state.animateScrollToPage(days) }
+                        },
+                    fontWeight = if (state.currentPage == days) {
+                        FontWeight.Bold
+                    } else {
+                        FontWeight.Normal
+                    },
+                    maxLines = 1,
+                    textAlign = TextAlign.Center
+                )
+            }
+
         }
     }
 
