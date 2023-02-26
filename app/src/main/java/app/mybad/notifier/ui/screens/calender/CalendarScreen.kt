657b284d85d57c316623f3715c12314d3ca36891
v <92236559+palevominimale@users.noreply.github.com>
2023-02-26 15:05:28 +0100

    updated Colors, implemented bottom modal for CalendarScreen.kt

--- app/src/main/java/app/mybad/notifier/ui/screens/calender/CalendarScreen.kt
+++ app/src/main/java/app/mybad/notifier/ui/screens/calender/CalendarScreen.kt
@@ -1,13 +1,21 @@
 package app.mybad.notifier.ui.screens.calender
 
+import android.util.Log
 import androidx.compose.animation.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckBox
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.RadioButtonChecked
+import androidx.compose.material.icons.filled.RadioButtonUnchecked
 import androidx.compose.material3.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -38,6 +46,9 @@ import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.util.SortedMap
+import java.util.TimeZone
 
 val coursesList = listOf(
     CourseDomainModel(id=1L, medId = 1L, startDate = 0L, endDate = 11000000L),
@@ -52,25 +63,34 @@ val medsList = listOf(
 )
 
 val usages = listOf(
-    UsageDomainModel(1677182681L),
-    UsageDomainModel(1677182681L),
-    UsageDomainModel(1677254681L),
-    UsageDomainModel(1677269081L),
-    UsageDomainModel(1677355481L),
-    UsageDomainModel(1677341081L),
-    UsageDomainModel(1677427481L),
+    UsageDomainModel(1677182682L),
+    UsageDomainModel(1677182683L),
+    UsageDomainModel(1677254684L),
+    UsageDomainModel(1677269085L),
+    UsageDomainModel(1677355486L),
+    UsageDomainModel(1677341087L),
+    UsageDomainModel(1677427488L),
+)
+val usages1 = listOf(
+    UsageDomainModel(1677182662L),
+    UsageDomainModel(1677182663L),
+    UsageDomainModel(1677254664L),
+    UsageDomainModel(1677269065L),
+    UsageDomainModel(1677355466L),
+    UsageDomainModel(1677341067L),
+    UsageDomainModel(1677427468L),
 )
 val usages2 = listOf(
     UsageDomainModel(1677182681L),
-    UsageDomainModel(1677182681L),
-    UsageDomainModel(1677254681L),
-    UsageDomainModel(1677269081L),
-    UsageDomainModel(1677355481L),
+    UsageDomainModel(1677182682L),
+    UsageDomainModel(1677254683L),
+    UsageDomainModel(1677269084L),
+    UsageDomainModel(1677355485L),
 )
 
 val usagesList = listOf(
     UsagesDomainModel(medId = 1L, usages = usages),
-    UsagesDomainModel(medId = 2L, usages = usages),
+    UsagesDomainModel(medId = 2L, usages = usages1),
     UsagesDomainModel(medId = 3L, usages = usages2),
 )
 
@@ -122,20 +142,18 @@ fun CalendarScreen(
                     scope.launch {  }
                 }
             )
-
-            if(dialogIsShown) {
-                BottomSlideInDialog(
-                    onDismissRequest = { dialogIsShown = false }
-                ) {
-                    Surface(
-                        shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
-                        color = MaterialTheme.colorScheme.background,
-                        elevation = 5.dp,
-                        modifier = Modifier.fillMaxWidth().height(350.dp)
-                    ) {
-
-                    }
-                }
+            if(dialogIsShown) BottomSlideInDialog(onDismissRequest = { dialogIsShown = false }) {
+                val daily = collectUsages(
+                    date = selectedDate,
+                    usages = usages,
+                    meds = meds
+                )
+                DailyUsages(
+                    date = selectedDate,
+                    monsterData = daily,
+                    meds = meds,
+                    onDismiss = { dialogIsShown = false }
+                )
             }
 
         }
@@ -317,7 +335,7 @@ private fun CalendarDayItem(
                 CircleShape
             )
             .alpha(if (isOtherMonth) 0.5f else 1f)
-            .clickable { onSelect(date) }
+            .clickable { if(usages>0) onSelect(date) }
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
@@ -337,6 +355,147 @@ private fun CalendarDayItem(
                 style = Typography.bodyLarge,
                 color = if(isSelected) MaterialTheme.colorScheme.primary else Color.Unspecified
             )
+        }
+    }
+}
+
+@Composable
+private fun DailyUsages(
+    modifier: Modifier = Modifier,
+    date: LocalDateTime?,
+    meds: List<MedDomainModel>,
+    monsterData: List<Pair<Long, Long>>,
+    onDismiss: () -> Unit = {},
+    onNextDay: () -> Unit = {},
+    onPrevDay: () -> Unit = {},
+) {
+    Surface(
+        shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
+        color = MaterialTheme.colorScheme.background,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = date?.format(DateTimeFormatter.ofPattern("dd MMMM")) ?: "no date",
+                    style = Typography.titleLarge,
+                )
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(20.dp)
+                        .clickable(
+                            indication = null,
+                            interactionSource = MutableInteractionSource(),
+                            onClick = onDismiss::invoke
+                        )
+                )
+            }
+            Divider(
+                thickness = 1.dp,
+                color = MaterialTheme.colorScheme.outline,
+                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp, horizontal = 4.dp)
+            )
+            LazyColumn {
+                monsterData.sortedBy { it.first }.forEach { entry ->
+                    item { SingleUsageItem(date = entry.first, med = meds.first{ it.id == entry.second } ) }
+                }
+            }
+        }
+    }
+}
+
+private fun collectUsages(
+    date: LocalDateTime?,
+    usages: List<UsagesDomainModel>,
+    meds: List<MedDomainModel>
+) : List<Pair<Long, Long>> {  //time to medId
+
+    val res = mutableListOf<Pair<Long, Long>>()
+    usages.forEach { singleUsages ->
+        singleUsages.usages.forEach {
+            val itsTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(it.timeToUse), ZoneId.systemDefault())
+            if(itsTime.year == date?.year && itsTime.dayOfYear == date.dayOfYear) {
+                res.add(Pair(it.timeToUse, singleUsages.medId))
+            }
+        }
+    }
+    res.forEach { (time, medDomainModels) ->
+        Log.w("CS_", " $time : $medDomainModels ")
+    }
+    return res
+}
+
+@Composable
+private fun SingleUsageItem(
+    modifier: Modifier = Modifier,
+    date: Long,
+    med: MedDomainModel,
+    isTaken: Boolean = false
+) {
+    val units = stringArrayResource(R.array.units)
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        val time = LocalDateTime
+            .ofInstant(Instant.ofEpochSecond(date), ZoneId.systemDefault())
+            .format(DateTimeFormatter.ofPattern("HH:mm"))
+        Text(text = time)
+        Surface(
+            color = MaterialTheme.colorScheme.primaryContainer,
+            shape = RoundedCornerShape(10.dp),
+            elevation = 1.dp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth().padding(8.dp)
+            ) {
+                Icon(
+                    painter = painterResource(med.details.icon),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(end = 8.dp)
+                        .size(40.dp)
+                )
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                        .weight(1f)
+                ) {
+                    Text(
+                        text = "${med.name}, ${med.details.dose} ${units[med.details.measureUnit]}",
+                        style = Typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "2 pcs", style = Typography.labelMedium)
+                    }
+                }
+                Icon(
+                    imageVector = if(isTaken) Icons.Default.RadioButtonChecked else Icons.Default.RadioButtonUnchecked,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                        .size(40.dp)
+                )
+            }
         }
     }
 }
