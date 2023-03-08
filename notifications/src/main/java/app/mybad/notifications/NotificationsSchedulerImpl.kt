086b4f0183e781a09d44e155b5eb8513498dcc93
v <92236559+palevominimale@users.noreply.github.com>
2023-03-08 15:13:39 +0100

    removed logs

--- notifications/src/main/java/app/mybad/notifications/NotificationsSchedulerImpl.kt
+++ notifications/src/main/java/app/mybad/notifications/NotificationsSchedulerImpl.kt
@@ -6,14 +6,10 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.icu.util.Calendar
-import android.util.Log
 import app.mybad.domain.models.usages.UsageCommonDomainModel
 import app.mybad.domain.repos.MedsRepo
 import app.mybad.domain.repos.UsagesRepo
 import app.mybad.domain.scheduler.NotificationsScheduler
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
 import javax.inject.Inject
 
 class NotificationsSchedulerImpl @Inject constructor(
@@ -26,7 +22,6 @@ class NotificationsSchedulerImpl @Inject constructor(
     private val calendar = Calendar.getInstance()
     @SuppressLint("UnspecifiedImmutableFlag")
     override suspend fun add(usages: List<UsageCommonDomainModel>) {
-        Log.w("NSI_now", "${LocalDateTime.ofInstant(Instant.ofEpochMilli(System.currentTimeMillis()), ZoneId.systemDefault())}")
         usages.forEach {
             val i = Intent(context, AlarmReceiver::class.java)
             val med = medsRepo.getSingle(it.medId)
@@ -35,7 +30,6 @@ class NotificationsSchedulerImpl @Inject constructor(
             i.putExtra("unit", med.measureUnit)
             val pi = PendingIntent.getBroadcast(context, 0, i, 0)
             calendar.timeInMillis = it.useTime*1000
-            Log.w("NSI_datetime", "${LocalDateTime.ofInstant(Instant.ofEpochMilli(calendar.timeInMillis), ZoneId.systemDefault())}")
             alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pi)
         }
     }
