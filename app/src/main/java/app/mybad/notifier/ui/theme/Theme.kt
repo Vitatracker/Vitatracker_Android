e2984fa86766d854adc87b473c497732b6fa0db6
v <92236559+palevominimale@users.noreply.github.com>
2023-04-08 19:12:46 +0200

    colors rolled back

--- app/src/main/java/app/mybad/notifier/ui/theme/Theme.kt
+++ app/src/main/java/app/mybad/notifier/ui/theme/Theme.kt
@@ -41,6 +41,60 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+//private val DarkColorScheme = darkColorScheme(
+//    primary = primary80,
+//    onPrimary = primary20,
+//    primaryContainer = primary30,
+//    onPrimaryContainer = primary90,
+//    secondary = secondary80,
+//    onSecondary = secondary20,
+//    secondaryContainer = secondary30,
+//    onSecondaryContainer = secondary90,
+//    tertiary = tertiary80,
+//    onTertiary = tertiary20,
+//    tertiaryContainer = tertiary30,
+//    onTertiaryContainer = tertiary90,
+//    error = error80,
+//    onError = error20,
+//    errorContainer = error30,
+//    onErrorContainer = error90,
+//    outline = neutralVariant60,
+//    outlineVariant = neutralVariant60,
+//    surfaceVariant = neutralVariant30,
+//    onSurfaceVariant = neutralVariant80,
+//    background = neutral10,
+//    onBackground = neutral90,
+//    surface = neutral10,
+//    onSurface = neutral80
+//)
+//
+//private val LightColorScheme = lightColorScheme(
+//    primary = primary40,
+//    onPrimary = primary100,
+//    primaryContainer = primary90,
+//    onPrimaryContainer = primary10,
+//    secondary = secondary40,
+//    onSecondary = secondary100,
+//    secondaryContainer = secondary90,
+//    onSecondaryContainer = secondary10,
+//    tertiary = tertiary40,
+//    onTertiary = tertiary100,
+//    tertiaryContainer = tertiary90,
+//    onTertiaryContainer = tertiary10,
+//    error = error40,
+//    onError = error100,
+//    errorContainer = error90,
+//    onErrorContainer = error10,
+//    outline = neutralVariant50,
+//    outlineVariant = neutralVariant50,
+//    surfaceVariant = neutralVariant90,
+//    onSurfaceVariant = neutralVariant30,
+//    background = neutral99,
+//    onBackground = neutral10,
+//    surface = neutral99,
+//    onSurface = neutral10
+//)
+
 @Composable
 fun MyBADTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
