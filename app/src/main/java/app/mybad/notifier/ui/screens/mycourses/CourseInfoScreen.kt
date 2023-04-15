03c30e4b0ca415b4fe57dbb5120b94cae9b39898
v <92236559+palevominimale@users.noreply.github.com>
2023-04-15 20:49:00 +0200

    fix: removed unnecessary items deploy it bro

--- app/src/main/java/app/mybad/notifier/ui/screens/mycourses/CourseInfoScreen.kt
+++ app/src/main/java/app/mybad/notifier/ui/screens/mycourses/CourseInfoScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -15,7 +14,6 @@ import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -39,9 +37,7 @@ private val usagesPattern = listOf<Pair<Long, Int>>(
     Pair(1678204800, 2)
 )
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
-@Preview(showBackground = true)
 fun CourseInfoScreen(
     modifier: Modifier = Modifier,
     course: CourseDomainModel = CourseDomainModel(),
@@ -84,7 +80,7 @@ fun CourseInfoScreen(
     }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween,
     ) {
         Column {
