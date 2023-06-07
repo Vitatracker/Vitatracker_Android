e38aebb22a3a9b8b6b9795e1686f7ea8d20cef85
Ershov4O <95ersh@mail.ru>
2023-06-07 23:58:37 +1000

    доработки окна "Контакты"

--- app/src/main/java/app/mybad/notifier/ui/screens/settings/about/SettingsOurContacts.kt
+++ app/src/main/java/app/mybad/notifier/ui/screens/settings/about/SettingsOurContacts.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -22,10 +19,8 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import app.mybad.notifier.BuildConfig
 import app.mybad.notifier.R
 import app.mybad.notifier.ui.screens.settings.common.BaseDivider
-import app.mybad.notifier.ui.screens.settings.common.SettingsAboutItem
 
 @Composable
 fun SettingsOurContacts(modifier: Modifier = Modifier) {
