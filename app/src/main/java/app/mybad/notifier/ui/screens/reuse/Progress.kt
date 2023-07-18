553b581f740ab2e3141ea6d26b6baedc7d5d99e9
Ershov4O <95ersh@mail.ru>
2023-07-18 22:57:33 +1000

    refactoring

--- app/src/main/java/app/mybad/notifier/ui/screens/reuse/Progress.kt
+++ app/src/main/java/app/mybad/notifier/ui/screens/reuse/Progress.kt
@@ -2,8 +2,7 @@ package app.mybad.notifier.ui.screens.reuse
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,9 +15,7 @@ fun Progress() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-        CircularProgressIndicator(
-            color = MaterialTheme.colors.primary
-        )
+        CircularProgressIndicator()
     }
 }
 
